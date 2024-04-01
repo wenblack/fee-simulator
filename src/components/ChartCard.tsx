33ced0402e369eddc30f4ab8b4b55a5b6d0fe883
Wender Barbosa <wenblack012@gmail.com>
2024-04-01 16:14:43 -0300

    ✨ feat: Export ChartCard
    Export/use ChartCard Component on HomePage

--- src/components/ChartCard.tsx
+++ src/components/ChartCard.tsx
@@ -1,41 +1,36 @@
-"use client";
-import { useEffect } from "react";
 import Chart from "react-google-charts";
 
 interface ChartProps {
+  name: string;
   investido: number;
   totalJuros: number;
   totalGeral: number;
 }
 
-export function ChartCard({ investido, totalGeral, totalJuros }: ChartProps) {
+const BRL = new Intl.NumberFormat("pt-BR", {
+  style: "currency",
+  currency: "BRL",
+});
+export function ChartCard({
+  name,
+  investido,
+  totalGeral,
+  totalJuros,
+}: ChartProps) {
   let data = [
-    ["", "Investido", "Juros", "Total"],
-    ["2014", investido, totalJuros, totalGeral],
+    ["", "Juros", "Investido", "Geral"],
+    [
+      name,
+      BRL.format(totalJuros),
+      BRL.format(investido),
+      BRL.format(totalGeral),
+    ],
   ];
   const options = {
     chart: {
       title: "Resultado da Simulação",
     },
   };
-  function calculate(
-    aporteInicial: number,
-    aporteMensal: number,
-    taxaJurosMensal: number,
-    tempoMeses: number
-  ) {
-    let taxaDecimal = taxaJurosMensal / 100;
-    let montanteFinal = aporteInicial * Math.pow(1 + taxaDecimal, tempoMeses);
-    montanteFinal +=
-      aporteMensal *
-      ((Math.pow(1 + taxaDecimal, tempoMeses) - 1) / taxaDecimal);
-    montanteFinal = Math.round(montanteFinal * 100) / 100;
-
-    console.log("Montante final após", tempoMeses, " meses:", montanteFinal);
-  }
-  useEffect(() => {
-    calculate(500, 1000, 1, 12);
-  }, []);
   if (
     totalGeral === undefined ||
     totalJuros === undefined ||
